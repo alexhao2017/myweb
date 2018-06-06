1ca99c4d1196d1dfb648b18dd740e6ac3f0898e0
Muzi <muzi@MuzimatoMacBook-Pro.local>
2018-06-06 23:49:30 +1000

    -a

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
-import * as contentful from 'contentful';
-var client = contentful.createClient({
-  space: 'xche690pbrqv',
-  accessToken: 'b116f8156897c9c7ff3bfd3fd9c2c3148b694fd1244a65b2ddccf23fbb53e3a2' })
-client.getEntries().then(entries => {
-  entries.items.forEach(entry => {
-    if(entry.fields) {
-      console.log(entry.fields)
-    }
-  })
-})
+
 ReactDOM.render(<Router>
     <App />
   </Router>, document.getElementById('root'));
